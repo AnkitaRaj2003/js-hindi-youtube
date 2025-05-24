3aab8cf16988d105a57b2e793d7ed2a32a682e84
Ankita meena <104414458+AnkitaRaj2003@users.noreply.github.com>
2025-05-24 18:20:11 +0530

    Update 06_nums_and_math.js

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -5,7 +5,7 @@ const balance = new Number(100)
 // console.log(balance);
 
 // console.log(balance.toString().length);
-// console.log(balance.toFixed(1));
+// console.log(balance.toFixed(1)); - It returns a string representing the number rounded to the specified number of digits after the decimal point.
 
 const otherNumber = 123.8966
 
