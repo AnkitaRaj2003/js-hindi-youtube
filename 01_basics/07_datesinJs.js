b2bc7cadbcc214bcad01f95eb019abdea3b9919e
Ankita meena <104414458+AnkitaRaj2003@users.noreply.github.com>
2025-05-24 18:40:29 +0530

    Update 07_datesinJs.js

--- 01_basics/07_datesinJs.js
+++ 01_basics/07_datesinJs.js
@@ -4,8 +4,9 @@ let myDate = new Date()
 // console.log(myDate.toString());
 // console.log(myDate.toDateString());
 // console.log(myDate.toLocaleString());
-// console.log(typeof myDate);
+// console.log(typeof myDate); //Object
 
+// Declaring our own date
 // let myCreatedDate = new Date(2023, 0, 23)
 // let myCreatedDate = new Date(2023, 0, 23, 5, 3)
 // let myCreatedDate = new Date("2023-01-14")
