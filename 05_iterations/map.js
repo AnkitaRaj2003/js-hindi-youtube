3e541197c6b2ad532eddb19748b7c0812d587acb
Ankita meena <104414458+AnkitaRaj2003@users.noreply.github.com>
2025-05-30 13:41:10 +0530

    Update and rename seven.js to map.js

--- 05_iterations/map.js
+++ 05_iterations/map.js
@@ -1,3 +1,4 @@
+// The map() method of Array instances creates a new array populated with the results of calling a provided function on every element in the calling array.
 const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 // const newNums = myNumers.map( (num) => { return num + 10})
