c6cd9c5afda88ff3bba747ea7e7141fcd55a4675
Ankita meena <104414458+AnkitaRaj2003@users.noreply.github.com>
2025-05-31 20:34:17 +0530

    Update promises.js

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -104,6 +104,11 @@ consumePromiseFive()
 
 //getAllUsers()
 
+
+// The fetch() method of the Window interface starts the process of fetching a resource from the network, returning a promise that is fulfilled once the response is available.
+// The promise resolves to the Response object representing the response to your request.
+// A fetch() promise only rejects when the request fails, for example, because of a badly-formed request URL or a network error. A fetch() promise does not reject if the server responds with HTTP status codes that indicate errors (404, 504, etc.). Instead, a then() handler must check the Response.ok and/or Response.status properties.
+
 fetch('https://api.github.com/users/hiteshchoudhary')
 .then((response) => {
     return response.json()
